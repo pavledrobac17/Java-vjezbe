dd9e3089fd6f75facc1931f1690be4f21b8abb67
pavledrobac17 <pavledrobac17@gmail.com>
2025-10-02 11:25:01 +0200

    Add files via upload

---
+++ Enemy.java
@@ -0,0 +1,20 @@
+package com.example.myapp;
+
+public class Enemy {
+	int x;
+    int y;
+    double width;
+    double height;
+    double health;
+    public C(int x, int y, double width, double height, double health){
+    	this.x = x;           
+        this.y = y;           
+        this.width = width;   
+        this.height = height;
+        this.health = health; 	
+    }
+ 
+}
+//pavle drobac 24025
+//luka cirovic 24024
+//filip krunic 24085
---
+++ Playerr.java
@@ -0,0 +1,26 @@
+package com.example.myapp;
+
+public class Playerr {
+	
+	public static void main(String[] args) {
+		 // Atributi
+	    int x;
+	    int y;
+	    double width;
+	    double height;
+	    double health;
+	    
+	      public C(int x, int y, double width, double height, double health) {
+	    	
+	    }
+	    this.x = x;       
+        this.y = y;          
+        this.width = width;   
+        this.height = height; 
+        this.health = health; 
+		
+		
+
+	}
+
+}
